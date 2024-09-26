4d1b489e3093d36f71ee6c650e77a2017d2b1bdb
Simon Oulevay <simon.oulevay@gmail.com>
2024-09-26 22:36:32 +0200

    Fix injection & xss vulnerabilities

--- index.php
+++ index.php
@@ -26,8 +26,9 @@ if (isset($_POST['action'])) {
 
       $title = $_POST['title'];
       if ($title && $title !== '') {
-        $insertQuery = 'INSERT INTO todo VALUES(NULL, \''.$title.'\', FALSE, CURRENT_TIMESTAMP)';
-        if (!$db->query($insertQuery)) {
+        $insert = $db->prepare('INSERT INTO todo VALUES(NULL, :title, FALSE, CURRENT_TIMESTAMP)');
+        $insert->bindParam('title', $title);
+        if (!$insert->execute()) {
           die(print_r($db->errorInfo(), true));
         }
       }
@@ -43,9 +44,10 @@ if (isset($_POST['action'])) {
 
       $id = $_POST['id'];
       if(is_numeric($id)) {
-        $updateQuery = 'UPDATE todo SET done = !done WHERE id = ' . $id;
-        if(!$db->query($updateQuery)) {
-          die(print_r($db->errorInfo(), true));
+        $update = $db->prepare('UPDATE todo SET done = NOT done WHERE id = :id');
+        $update->bindParam(':id', $id, PDO::PARAM_INT);
+        if(!$update->execute()) {
+          die(print_r($update->errorInfo(), true));
         }
       }
 
@@ -59,9 +61,10 @@ if (isset($_POST['action'])) {
 
       $id = $_POST['id'];
       if(is_numeric($id)) {
-        $deleteQuery = 'DELETE FROM todo WHERE id = ' . $id;
-        if(!$db->query($deleteQuery)) {
-          die(print_r($db->errorInfo(), true));
+        $delete = $db->prepare('DELETE FROM todo WHERE id = :id');
+        $delete->bindParam(':id', $id, PDO::PARAM_INT);
+        if(!$delete->execute()) {
+          die(print_r($delete->errorInfo(), true));
         }
       }
 
@@ -131,7 +134,7 @@ $items = $db->query($selectQuery);
         <?php foreach($items as $item): ?>
           <div class='list-group-item d-flex justify-content-between align-items-center<?php if($item['done']): ?> list-group-item-success<?php else: ?> list-group-item-warning<?php endif;?>'>
 
-            <div class='title'><?= $item['title'] ?></div>
+            <div class='title'><?= htmlspecialchars($item['title']) ?></div>
 
             <!-- Todo item controls -->
             <form action='<?= BASE_URL ?>' method='post'>
